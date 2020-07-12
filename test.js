3c6e96ddf6f1f09c14f2b47721746a2ba1c3f61d
gayathri <gayathriartist2000@gmail.com>
2020-07-13 00:28:36 +0530

    test page designed

---
+++ test.js
@@ -0,0 +1,60 @@
+var question =[
+{
+    title:"Grand Central Terminal, Park Avenue, New York is the world's",
+    option: ["largest railway station","longest railway station ","highest railway station "]
+},
+
+{
+    title:"Entomology is the science that studies ",
+    option:["Behavior of human beings ","Insects","The origin and history of technical and scientific terms "]
+},
+
+
+{
+    title:"The origin and history of technical and scientific terms ",
+    option:["Africa","Europe","Australia"]
+},
+
+{
+    title:"Garampani sanctuary is located at",
+    option:["Diphu, Assam","Gangtok, Sikkim ","Junagarh, Gujarat "]
+},
+
+
+{
+    title:"Golden Temple, Amritsar is India's ",
+    option:["Both option A and B are correct","oldest Gurudwara","largest Gurdwara "]
+},
+
+
+{
+    title:"Durand Cup is associated with the game of ",
+    option:["Football","Volleyball ","Cricket"]
+},
+
+
+{
+    title:"Hermann Scheer (Germany) received right Livelihood Award in 1999 for ",
+    option:["his long standing efforts to end the impunity of dictators","his indefatigable work for thepromotion of solar energy worldwide","his long standing efforts to end the impunity of dictators    "]
+},
+
+
+{
+    title:"During World War II, when did Germany attack France?",
+    option:["1940","1946 ","1945"]
+},
+
+
+{
+    title:"Federation Cup, World Cup, Allywyn International Trophy and Challenge Cup are awarded to winners of ",
+    option:["Basketball","Tennis ","Volleyball"]
+},
+
+
+{
+    title:"Gulf cooperation council was originally formed by",
+    option:["Third World Nations","Fourth World Nations","Second World Nations"]
+}
+
+
+]
