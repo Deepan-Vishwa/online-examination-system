00f17213ca83360e2312d4231b06535ea4af1ed4
shobica18 <59160970+shobica18@users.noreply.github.com>
2020-07-17 21:18:26 +0530

    merged instruct- test page with full screen

---
+++ js/test.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $("#start").click(function(){
+        launchIntoFullscreen(document.documentElement);
+        
+        console.log("1");
+     $("#title").hide();
+     $("#instruction").hide();
+     $("#nav").show();
+     $("#question").show();
+
+    
+    });
+$(document.documentElement).keydown(function(){
+
+  alert("key pressed");
+  
+});
+
+
+
+
+    function launchIntoFullscreen(element) {
+        if(element.requestFullscreen) {
+          element.requestFullscreen();
+      } else if(element.mozRequestFullScreen) {
+          element.mozRequestFullScreen();
+        } else if(element.webkitRequestFullscreen) {
+          element.webkitRequestFullscreen();
+        } else if(element.msRequestFullscreen) {
+          element.msRequestFullscreen();
+        }
+      }
+});
