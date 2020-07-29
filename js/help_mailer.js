a45c6b7458ca630f40a34c2d23b0a182c7eab4ff
Kiruthiga <kiruthigait2000@gmail.com>
2020-07-29 22:22:32 +0530

    help_mailer

--- js/help_mailer.js
+++ js/help_mailer.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
     
-
-
-    
     $("#send_mail").click(function(){
+
+        $("#send_mail").attr("disabled",true);
+        $("#send_mail").html('<span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true" id="load"></span>Sending...');
+
         var email = $("#help_email").val().trim();
          var ci = $("#help_ci").val().trim();
      var oi = $("#help_oi").val().trim();
@@ -19,13 +20,56 @@ $(document).ready(function(){
                 action: "login_mail"
             },
             success: function(dataResult){
-               alert(dataResult);
+                $("#login_mail_modal").modal('show');
+                $("#login_mail_modal_body").text(dataResult);
+ 
+                $("#load").remove();
+               $("#send_mail").attr("disabled",false);
+               $("#send_mail").html("Send");
             }
            
-        })
-    });
+        });
+        
+     });
 
 
 
+     
+    $("#main_mail").click(function(){
+        $("#main_mail").attr("disabled",true);
+        $("#main_mail").html('<span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true" id="load"></span>Sending...');
+         var coi = $("#help_common").val().trim();
+     var oti = $("#help_other").val().trim();
+     var prd = $("#help_problem").val().trim();
+        $.ajax({ 
+            url: "mailer.php",
+            type: "POST",
+            data:{
+               
+                coi:coi,
+                oti:oti,
+                prd:prd,
+                action: "main_mail"
+            },
+            success: function(dataResult){
+              
+               $("#main_mail_modal").modal('show');
+               $("#main_mail_modal_body").text(dataResult);
+
+               $("#load").remove();
+              $("#main_mail").attr("disabled",false);
+              $("#main_mail").html("Send");
+            }
+           
+        });
+        
+     });
+
+
+    
 
 });
+
+     
+
+    
--- mailer.php
+++ mailer.php
@@ -5,32 +5,43 @@ session_start();
 
         if($action == "login_mail"){
 
-         $to = "srdeepansr@gmail.com";
+         $to = "kiruthigait2000@gmail.com,srdeepansr@gmail.com,gayathriartist2000@gmail.com";
          $subject = "Login Help";
          $message = 
          '
         
+         <style>
+         .font_res{
+            font-size: 1vw;
+         }
+         @media screen and (max-width: 700px) {
+            .font_res{
+                font-size: 3vw;
+             }
+            
+         }
          
-         <div style="text-align:center; color:darkblue; font-size: 1vw;">
+         </style>
+         <div class="font_res" style="text-align:center; color:darkblue;">
           <h1>KDSG Examination System</h1>
           <img src="https://www.freeiconspng.com/uploads/help-icon-26.png" width="200" alt="HELP" padding-bottom:0;/>
         </div>
-        <table style="border: 1px solid blue; margin-left: auto; margin-right: auto; font-size: 1vw; background-color: aliceblue;">
-            <tr style="border: 1px solid darkblue;">
-                <th style="text-align: left;border: 1px solid blue;">Email</th>
-                <td style="padding-left: 20px;border: 1px solid blue;">'.$email.'</td>
+        <table class="font_res" style="margin-left: auto;border-collapse: collapse; margin-right: auto; background-color: aliceblue; width:90%; height:auto;">
+            <tr style="border: 1px solid greeen;">
+                <th style="text-align: left;border: 1px solid green;">Email</th>
+                <td style="padding-left: 20px;border: 1px solid green;">'.$email.'</td>
             </tr>
-            <tr style="border: 1px solid blue;">
-                <th style="text-align: left;border: 1px solid blue;">Common Issue</th>
-                <td style="padding-left: 20px;border: 1px solid blue;">'.$ci.'</td>
+            <tr style="border: 1px solid green;">
+                <th style="text-align: left;border: 1px solid green;">Common Issue</th>
+                <td style="padding-left: 20px;border: 1px solid green;">'.$ci.'</td>
             </tr>
-            <tr style="border: 1px solid blue;">
-                <th style="text-align: left;border: 1px solid blue;">other issue</th>
-                <td style="padding-left: 20px;border: 1px solid blue;">'.$oi.'</td>
+            <tr style="border: 1px solid green;">
+                <th style="text-align: left;border: 1px solid green;">Other Issue</th>
+                <td style="padding-left: 20px;border: 1px solid green;">'.$oi.'</td>
             </tr>
-            <tr style="border: 1px solid blue;">
-                <th style="text-align: left;border: 1px solid blue;">problem desc</th>
-                <td style="padding-left: 20px;border: 1px solid blue;">'.$pd.'</td>
+            <tr style="border: 1px solid green;">
+                <th style="text-align: left;border: 1px solid green;">Problem Description</th>
+                <td style="padding-left: 20px;border: 1px solid green;">'.$pd.'</td>
             </tr>
         </table>
         
@@ -48,45 +59,62 @@ session_start();
 
     if($action == "main_mail"){
 
+        $query ="
+        SELECT student_id,student_name,student_email FROM students where student_id=".$_SESSION['userid']." 
+        ";
+        $result = mysqli_query($conn, $query); 
+        while($row = mysqli_fetch_assoc($result))
+        {
+            $email = $row["student_email"];
+            $name = $row["student_name"];
+        }
+        
 
-        // $student_name = '';
-        // $student_mail = '';
-        // $studet_id = '';
-
-        $to = "srdeepansr@gmail.com";
-         $subject = "Login Help";
+        $to ="kiruthigait2000@gmail.com,srdeepansr@gmail.com,gayathriartist2000@gmail.com";
+         $subject = "Student Help Request";
          $message = 
          '
         
+         <style>
+         .font_res{
+            font-size: 1vw;
+         }
+         @media screen and (max-width: 700px) {
+            .font_res{
+                font-size: 3vw;
+             }
+            
+         }
          
-         <div style="text-align:center; color:darkblue; font-size: 1vw;">
+         </style>
+         <div class="font_res" style="text-align:center; color:darkblue;">
           <h1>KDSG Examination System</h1>
           <img src="https://www.freeiconspng.com/uploads/help-icon-26.png" width="200" alt="HELP" padding-bottom:0;/>
         </div>
-        <table style="border: 1px solid blue; margin-left: auto; margin-right: auto; font-size: 1vw; background-color: aliceblue;">
-            <tr style="border: 1px solid darkblue;">
-                <th style="text-align: left;border: 1px solid blue;">Email</th>
-                <td style="padding-left: 20px;border: 1px solid blue;">'.$email.'</td>
+        <table class="font_res" style="margin-left: auto;border-collapse: collapse; margin-right: auto; background-color: aliceblue; width:90%; height:auto;">
+            <tr style="border: 2px solid green;">
+                <th style="text-align: left;border: 2px solid green;">Name</th>
+                <td style="padding-left: 20px;border: 2px solid green;">'.$name.'</td>
             </tr>
-            <tr style="border: 1px solid darkblue;">
-            <th style="text-align: left;border: 1px solid blue;">Email</th>
-            <td style="padding-left: 20px;border: 1px solid blue;">'.$email.'</td>
+            <tr style="border: 2px solid darkblue;">
+            <th style="text-align: left;border: 2px solid green;">Reg No</th>
+            <td style="padding-left: 20px;border: 2px solid green;">'.$_SESSION["userid"].'</td>
         </tr>
-        <tr style="border: 1px solid darkblue;">
-        <th style="text-align: left;border: 1px solid blue;">Email</th>
-        <td style="padding-left: 20px;border: 1px solid blue;">'.$email.'</td>
+        <tr style="border: 2px solid darkblue;">
+        <th style="text-align: left;border: 2px solid green;">Email</th>
+        <td style="padding-left: 20px;border: 2px solid green;">'.$email.'</td>
     </tr>
-            <tr style="border: 1px solid blue;">
-                <th style="text-align: left;border: 1px solid blue;">Common Issue</th>
-                <td style="padding-left: 20px;border: 1px solid blue;">'.$ci.'</td>
+            <tr style="border: 2px solid green;">
+                <th style="text-align: left;border: 2px solid green;">Common Issue</th>
+                <td style="padding-left: 20px;border: 2px solid green;">'.$coi.'</td>
             </tr>
-            <tr style="border: 1px solid blue;">
-                <th style="text-align: left;border: 1px solid blue;">other issue</th>
-                <td style="padding-left: 20px;border: 1px solid blue;">'.$oi.'</td>
+            <tr style="border: 2px solid green;">
+                <th style="text-align: left;border: 2px solid green;">Other Issue</th>
+                <td style="padding-left: 20px;border: 2px solid green;">'.$oti.'</td>
             </tr>
-            <tr style="border: 1px solid blue;">
-                <th style="text-align: left;border: 1px solid blue;">problem desc</th>
-                <td style="padding-left: 20px;border: 1px solid blue;">'.$pd.'</td>
+            <tr style="border: 2px solid green;">
+                <th style="text-align: left;border: 2px solid green;">Problem Description</th>
+                <td style="padding-left: 20px;border: 2px solid green;">'.$prd.'</td>
             </tr>
         </table>
         
