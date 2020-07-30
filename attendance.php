dd3ae59338efad36eb9399c8f6ba3456b9a0fbbd
Deepan Vishwa S R <srdeepansr@outlook.com>
2020-07-31 00:26:07 +0530

    result validation and attendance

---
+++ attendance.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+extract($_POST);
+include 'config.php';
+
+$time =  date("Y-m-d H:i:s");
+
+$attendance_query = "INSERT INTO `attendance`(`online_exam_id`, `student_id`, `time_stamp`) 
+VALUES (".$_SESSION['online_exam_id'].",".$_SESSION['userid'].",'".$time."')";
+
+$conn->query($attendance_query);
+?>
