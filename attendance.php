8d43d5391a37bfd222a737f489a110f12787d1be
Deepan Vishwa S R <srdeepansr@outlook.com>
2020-08-01 18:28:50 +0530

    attendance timer bug fix

--- attendance.php
+++ attendance.php
@@ -2,7 +2,7 @@
 session_start();
 extract($_POST);
 include 'config.php';
-
+date_default_timezone_set('Asia/Kolkata');
 $time =  date("Y-m-d H:i:s");
 
 $attendance_query = "INSERT INTO `attendance`(`online_exam_id`, `student_id`, `time_stamp`) 
