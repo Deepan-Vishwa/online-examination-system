6c7d1ec1d53c55e529be66ecf6898a4a81bf6bfb
Deepan-Vishwa <64106151+Deepan-Vishwa@users.noreply.github.com>
2021-03-02 19:57:09 +0530

    Update mail_sender.php

--- mail_sender.php
+++ mail_sender.php
@@ -8,8 +8,8 @@ $mail = new PHPMailer;
 $mail->isSMTP();                                    
 $mail->Host = 'smtp.mailtrap.io';  
 $mail->SMTPAuth = true;                               
-$mail->Username = '157b401e40942d';                 
-$mail->Password = 'efe13063b9194d';                           
+$mail->Username = ''; // username                 
+$mail->Password = ''; // password                          
 $mail->SMTPSecure = 'tls';                            
 $mail->Port = 2525;                                    
 $mail->setFrom('3bc6ad5d6a-615ccf@inbox.mailtrap.io', 'KDSG Examination System');
