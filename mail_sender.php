7430e206524a5584a4f6661e2a90da9b9c0780df
Deepan Vishwa S R <srdeepansr@outlook.com>
2021-03-13 18:08:39 +0530

    mail

--- mail_sender.php
+++ mail_sender.php
@@ -8,13 +8,8 @@ $mail = new PHPMailer;
 $mail->isSMTP();                                    
 $mail->Host = 'smtp.mailtrap.io';  
 $mail->SMTPAuth = true;                               
-<<<<<<< HEAD
-$mail->Username = '6b5e609730b0c8';                 
-$mail->Password = '3f1bb6bc129388';                           
-=======
 $mail->Username = ''; // username                 
 $mail->Password = ''; // password                          
->>>>>>> 6c7d1ec1d53c55e529be66ecf6898a4a81bf6bfb
 $mail->SMTPSecure = 'tls';                            
 $mail->Port = 2525;                                    
 $mail->setFrom('3bc6ad5d6a-615ccf@inbox.mailtrap.io', 'KDSG Examination System');
