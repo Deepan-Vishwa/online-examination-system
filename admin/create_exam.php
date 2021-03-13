935eb175ab2e30a5b062f20599aeeabf57dc94f2
Deepan Vishwa S R <srdeepansr@outlook.com>
2021-03-13 19:12:57 +0530

    12hrs format

--- admin/create_exam.php
+++ admin/create_exam.php
@@ -141,8 +141,7 @@ include '../config.php';
                                         <input type="datetime-local" class="form-control" id="start_date"
                                             name="start_date" required>
                                         <script>
-                                        var today = new Date(new Date().getTime() - new Date().getTimezoneOffset() * 60000).toISOString();;
-                                        document.getElementById("start_date").min = today.slice(0,19);
+                                        document.getElementById("start_date").min = new Date(new Date().getTime() - new Date().getTimezoneOffset() * 60000).toISOString();
                                         </script>
 
                                     </div>
